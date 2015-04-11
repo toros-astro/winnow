44012d2ec5bb2c9a8ee934c193d8e56610920b82
Martin Beroiz <martinberoiz@phys.utb.edu>
2015-04-11 16:01:45 -0500

    Added real fetching of unranked transient candidates and present the user with those unranked. If all transients are ranked it will present the user those that are not ranked by himself. If none is available after that, it presents a message indicating so.

--- toros/toros/settings.py
+++ toros/toros/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'winnow'
+    'winnow',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
--- toros/winnow/urls.py
+++ toros/winnow/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^about/$', views.about, name='about'),
                        url(r'^rank/$', views.rank, name='rank'),
-                       url(r'^thumb/$', views.thumb, name='thumb'),
+                       url(r'^thumb/(?P<trans_candidate_id>\d+)/$', views.thumb, name = 'thumb'),
 )
