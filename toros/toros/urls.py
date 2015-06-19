c7a9f5081ae45f7ab4e8dc6e49362369afb5b67d
Martin Beroiz <martinberoiz@phys.utb.edu>
2015-06-18 19:49:46 -0500

    This removes the initial /training/ on every url. Use the web server (apache) to redirect /training to the web app, instead

--- toros/toros/urls.py
+++ toros/toros/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^training/', include("winnow.urls")),
+    url(r'^', include("winnow.urls")),
     url(r'^comments/', include('django_comments.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
