db7bd2392324653a8beffb5549cb93d224fec239
Martin Beroiz <martinberoiz@yahoo.com>
2015-12-22 18:55:03 -0600

    Fixed admin interface to work with Dataset table

--- torosweb/winnow/admin.py
+++ torosweb/winnow/admin.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
-from winnow.models import TransientCandidate, UserProfile, Ranking
+from winnow.models import TransientCandidate, UserProfile, Ranking, Dataset
 
 class TransientCandidateAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Sky Position", {'fields': ['ra', 'dec']}),
         ("Image Position", {'fields': ['x_pix', 'y_pix', 'width', 'height']}),
-        ("File Info", {'fields': ['filename', 'dataset_id', 'object_id', 'slug']}),
+        ("File Info", {'fields': ['filename', 'dataset', 'object_id', 'slug']}),
         ("Image Files", {'fields': ['refImg', 'origImg', 'subtImg']}),
-    ]            
+    ]
             
 class RankingAdmin(admin.ModelAdmin):
     list_display = ('ranker', 'rank', 'isInteresting')
 
+class DatasetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'isCurrent', 'notes')
+
 admin.site.register(UserProfile)
 admin.site.register(TransientCandidate, TransientCandidateAdmin)
 admin.site.register(Ranking, RankingAdmin)
+admin.site.register(Dataset, DatasetAdmin)
