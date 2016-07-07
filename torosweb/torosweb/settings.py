edc33df4ec76a3ac4d1c05046485392f62dfef69
Martin Beroiz <martinberoiz@yahoo.com>
2016-07-07 14:21:44 -0500

    restored database name to db.sqlite3

--- torosweb/torosweb/settings.py
+++ torosweb/torosweb/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'torosweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.with_rbmanager.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
