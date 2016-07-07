e8c0c8349e2aaec980e27ea45c78d9054b97fed4
Martin Beroiz <martinberoiz@yahoo.com>
2016-07-07 14:20:01 -0500

    RB index handles user for experiment.

--- torosweb/rbmanager/views.py
+++ torosweb/rbmanager/views.py
@@ -1,26 +1,44 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Experiment
 from .forms import ExperimentForm
 
+from winnow.models import UserProfile
+
 
 def index(request):
     if request.method == "POST":
+        notification_failed = {'value': True, 'type': 'danger',
+                               'message': 'The experiment could not be saved'}
+        notification_passed = {'value': True, 'type': 'success',
+                               'message': 'The experiment has been saved.'}
         exp_form = ExperimentForm(request.POST)
         if exp_form.is_valid():
             # Process form
-            new_exp = exp_form.instance
-            new_exp.user = User.objects.get(pk=1)
-            new_exp.save()
-            notification = {'value': True, 'type': 'success',
-                            'message': 'The experiment has been saved'}
-            exp_form = ExperimentForm()
-        else:
-            notification = {'value': True, 'type': 'danger',
-                            'message': 'The experiment could not be saved'}
+            if request.user.is_authenticated():
+                new_exp = exp_form.instance
+                try:
+                    user_prof = request.user.userprofile
+                except ObjectDoesNotExist:
+                    user_prof = UserProfile(user=request.user)
+                    user_prof.save()
+                    notification_passed['message'] += \
+                        " A new profile has been created for the user."
+                finally:
+                    new_exp.user = user_prof
+                    new_exp.save()
+                    notification = notification_passed
+                    exp_form = ExperimentForm()
+            else:  # if user is not authenticated
+                notification_failed['message'] = "You need to be logged in " \
+                    "to upload an experiment."
+                notification = notification_failed
 
-    if request.method == "GET":
+        else:  # if form is not valid:
+            notification = notification_failed
+
+    else:  # if request.method == "GET" or otherwise
         exp_form = ExperimentForm()
         notification = {'value': False, 'type': '', 'message': ''}
 
