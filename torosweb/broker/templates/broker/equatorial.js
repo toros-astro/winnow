3e510ff3286095bafc8445e5a94166971f96110b
Martin Beroiz <martinberoiz@gmail.com>
2017-08-25 15:26:25 -0500

    Added equatorial.js

---
+++ torosweb/broker/templates/broker/equatorial.js
@@ -0,0 +1,59 @@
+function raDeg2Hms(ra, round) {
+  var prefix = ''
+  var deg = +ra
+  if (deg < 0) {
+    prefix = '-'
+    deg = Math.abs(deg)
+  }
+  var hour = Math.floor(deg / 15)
+  var min = Math.floor(((deg / 15) - hour) * 60)
+  var sec = ((((deg / 15) - hour) * 60) - min) * 60
+  if (round) sec = Math.floor(sec)
+  return prefix + [hour, min, sec].join(':')
+}
+
+function decDeg2Hms(dec, round) {
+  var prefix = ''
+  var deg = +dec
+  if (deg < 0) {
+    prefix = '-'
+    deg = Math.abs(deg)
+  }
+  var hour = Math.floor(deg)
+  var min = Math.abs(Math.floor((deg - hour) * 60))
+  var sec = (Math.abs((deg - hour) * 60) - min) * 60
+  if (round) sec = Math.floor(sec)
+  return prefix + [hour, min, sec].join(':')
+}
+
+function raHms2Deg(ra, round) {
+  var parts = ra.split(':')
+  var sign = 1
+  var h = parseFloat(parts[0])
+  var m = parseFloat(parts[1])
+  var s = parseFloat(parts[2])
+  if (h.toString()[0] === '-') {
+    sign = -1
+    h = Math.abs(h)
+  }
+  var sDeg = (s / 240)
+  if (round) sDeg = Math.floor(sDeg)
+  deg = (h * 15) + (m / 4) + sDeg
+  return deg * sign
+}
+
+function decHms2Deg(dec, round) {
+  var parts = dec.split(':')
+  var sign = 1
+  var d = parseFloat(parts[0])
+  var m = parseFloat(parts[1])
+  var s = parseFloat(parts[2])
+  if (d.toString()[0] === '-') {
+    sign = -1
+    d = Math.abs(d)
+  }
+  var sDeg = (s / 3600)
+  if (round) sDeg = Math.floor(sDeg)
+  var deg = d + (m / 60) + sDeg
+  return deg * sign
+}
